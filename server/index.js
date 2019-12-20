bc9f8fbc3c377a333f42680d91803c58ca96d370
Akshay Dhanji <akshay.dhanji@powerweave.com>
2019-12-20 19:17:43 +0530

    first sync

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
+const router = express.Router();
+
+var urlParser = bodyParser.urlencoded({ extended: true });
+server.use(urlParser);
+server.use(bodyParser.json());
+
+server.use('/api', router);
+
+server.get('/', (req, res) => {
+    res.send(JSON.stringify({ welcome: 'Hello World' }));
+});
+
+router.route('/submitContact')
+.post((request, response) => {
+     response.setHeader( 'Content-Type', 'application/json' );
+     response.setHeader( 'Access-Control-Allow-Origin', '*' );
+    var res = JSON.stringify({
+        name: request.body.name,
+        email: request.body.email
+    });
+    console.log(res);
+    response.send(res);
+    
+});
+
+server.listen(3001, () => {
+    console.log('localhost is running on port 3001');
+})
+
+module.exports = router;
